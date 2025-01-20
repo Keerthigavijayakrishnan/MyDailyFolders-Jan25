036a8c9abc280c5db653867f39fec75d8acb9941
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-20 17:22:12 +0530

    Add files via upload
    Week1.Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		//Float
+		float browserVersion = 100.2f;
+	    System.out.println(browserVersion);
+		
+		//String
+		String browserName = "firefox";
+	    System.out.println(browserName);
+		
+		//Boolean
+		boolean isVisible = true;
+	    System.out.println(isVisible);
+		
+		//Integer
+		int releaseYear = 1998;
+	    System.out.println(releaseYear);
+		
+		//Character
+		char browserLogo = 'f';
+	    System.out.println(browserLogo);
+	    
+		//with space
+	 //   System.out.println (browserVersion + " " + browserName + " " + isVisible + " " + releaseYear + " " + browserLogo);
+	    //with next line
+	//	System.out.println (browserVersion +"\n" + browserName + "\n" + isVisible + "\n" + releaseYear + "\n" + browserLogo);
+	
+	}
+
+}
---
+++ day1/SampleClass.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+//Sample code
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleClass {
+	
+	public static void main(String[] args) {
+		
+		ChromeDriver driver =new ChromeDriver();
+	}
+
+}
