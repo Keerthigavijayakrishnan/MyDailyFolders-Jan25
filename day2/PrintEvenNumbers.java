f64cb18629ce58ee7d66a3d5ad82a4fb470c0393
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-22 16:59:36 +0530

    Add files via upload
    Week2.day2

---
+++ day2/PrintEvenNumbers.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class PrintEvenNumbers {
+	
+	public static void main(String[] args) {
+		 // To check the voter eligibility
+		
+/*	//Method 1:
+ 	int vote = 18;
+		int personAge = 0;
+		
+		if (personAge>=vote) {
+			System.out.println("Eligible to Vote");
+			} else {
+				System.out.println("Not Eligible to Vote");
+				}    */
+		
+	//	Method 2:
+			int age = 18;
+			if (age>=18) {
+				System.out.println("Eligible to Vote");
+			}else {
+				System.out.println("Not Eligible to Vote");
+			}           
+		
+	}
+}
