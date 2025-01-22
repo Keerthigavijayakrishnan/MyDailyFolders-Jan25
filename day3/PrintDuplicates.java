dce3897c3c03c8d07d090baced862602052d859c
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-22 17:02:45 +0530

    PrintDuplicates.java

--- day3/PrintDuplicates.java
+++ day3/PrintDuplicates.java
@@ -17,19 +17,19 @@ public class PrintDuplicates {
 		  
 		  //Method 1
 		  
-		  //Iterate all the values                      //length=8, length-1=7
-		  for (int i=0; i<num.length-1; i++) {          //0<7,1<7,2<7,3<7,4<7, 5<7,6>7, 7>7 (loops stop).if num.length=8,so 7>8 (loop continue)
+		  //Iterate all the values                      
+		  for (int i=0; i<num.length-1; i++) {          
 			  
 			  //Compare the value with same value
-			  if (num[i]==num[i+1]) {                  //2=2, 2=3, 3=5,5=5,5=7,7=7,7=9. If loop continue here, n[i]=9 but n[i+1}+].no value and outofboundexception error
+			  if (num[i]==num[i+1]) {                 
 				  System.out.println(num[i]);
 			  }
 		  }
 		  
 /*		  //Method 2
 	  
-         for (int i=0; i<num.length; i++) {         //0=2,1=2,2=3,3=5,4=5,5=7,6=7,7=9 (loop stop)
-         for (int j=i+1; j<num.length; j++)   {     //2355779
+         for (int i=0; i<num.length; i++) {        
+         for (int j=i+1; j<num.length; j++)   {     
            if (num[i]==num[j]) {                 
 				  System.out.println(num[i]);
 			  }
