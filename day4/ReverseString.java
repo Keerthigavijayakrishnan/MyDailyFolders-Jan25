4bcc7cac1652a090550576b9bd1a0b06bf5bd95a
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-23 14:05:21 +0530

    ReverseString.java

--- day4/ReverseString.java
+++ day4/ReverseString.java
@@ -12,9 +12,9 @@ public class ReverseString {
 		System.out.println("Reverse String: ");
 		
 		//Loop to iterate through each character of the string in reverse
-		for (int i=CharArray.length-1; i>0; i--) {         //0=T, 1=e, 2=s, 3=t, 4=l, 5=e, 6=a, 7=f
-			                                               //length=8, length(8)-1=7
-			System.out.println(CharArray[i]);              //CharArray[7]
+		for (int i=CharArray.length-1; i>0; i--) {         
+			                                              
+			System.out.println(CharArray[i]);              
 			
 		} 
 	     
@@ -23,12 +23,12 @@ public class ReverseString {
 
 		
 /*	//	Method2
-		String companyName = "TestLeaf";                     //0=T, 1=e, 2=s, 3=t, 4=l, 5=e, 6=a, 7=f
-		String rev ="";                                     //length=8, length(8)-1=7
-		for (int i=companyName.length()-1; i>=0; i--) {     //7,6,... (intial value executes only first time and inc/dec value starts executes from  second loop. if i=companyName.length()-1 executes i-- not executes. if i-- executes, i=companyName.length()-1 not executes.)
-			rev = rev+companyName.charAt(i);			    //0+CharAt[7]=f,...
+		String companyName = "TestLeaf";                     
+		String rev ="";                                     
+		for (int i=companyName.length()-1; i>=0; i--) {      
+			rev = rev+companyName.charAt(i);			   
 		}
-		System.out.println(rev);		                    //f,   
+		System.out.println(rev);		                    
 	}      
 
 }   */
