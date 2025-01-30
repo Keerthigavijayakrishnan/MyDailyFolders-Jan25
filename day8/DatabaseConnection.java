04685fc4a38ab9e6f7cf91e05d6ffb1b0e24ba60
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-30 17:19:12 +0530

    DatabaseConnection.java

--- day8/DatabaseConnection.java
+++ day8/DatabaseConnection.java
@@ -2,7 +2,7 @@ package week2.day4;
 
 public interface DatabaseConnection {
 	
-	//Abstract Methods or Unimplemented Methods (Only for declaring the methods/Maintaining the Structure)
+	//Abstract Methods/ Unimplemented Methods 
 	
 	public void connect();
 	
