d6be7dcf096efb31f3c10fa639e3388d93f277f5
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-02-14 12:07:54 +0530

    Add files via upload
    Week4.day1

---
+++ day 13/BaseClass.java
@@ -0,0 +1,36 @@
+package week4.day1.testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+	
+	ChromeDriver driver = new ChromeDriver();
+	
+	//Pre-conditions
+	
+	@BeforeMethod
+	public void preCondtions() {
+	//driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://leaftaps.com/opentaps/");
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+	driver.findElement(By.id("password")).sendKeys("crmsfa");
+	driver.findElement(By.className("decorativeSubmit")).click();
+	driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("Leads")).click();
+	}
+	
+	//Post-Conditions
+	
+	@AfterMethod
+	public void postCondtions() {
+		driver.close();
+	}
+
+}
